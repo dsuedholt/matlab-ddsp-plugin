0aa77b2a2531a6ba9709e1c22f7a906976946479
David Südholt <david.suedholt@gmail.com>
2021-05-20 11:38:45 +0200

    Add buildPlugins script

---
+++ buildPlugins.m
@@ -0,0 +1,13 @@
+% helper script to build plugins for each weight file
+
+validateAudioPlugin violinPlugin;
+generateAudioPlugin violinPlugin;
+
+validateAudioPlugin saxophonePlugin;
+generateAudioPlugin saxophonePlugin;
+
+validateAudioPlugin flutePlugin;
+generateAudioPlugin flutePlugin;
+
+validateAudioPlugin trumpetPlugin;
+generateAudioPlugin trumpetPlugin;
