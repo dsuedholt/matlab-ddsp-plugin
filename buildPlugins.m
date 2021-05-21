162f41fad5bdcdebae59321d9acfb8323905ef5c
David Südholt <david.suedholt@gmail.com>
2021-05-21 10:33:02 +0200

    Add commands to download weight files if not present

--- buildPlugins.m
+++ buildPlugins.m
@@ -1,14 +1,35 @@
 % helper script to build plugins for each weight file
 addpath('plugincode');
 
+
+if ~isfile('violinWeights.mat')
+    disp('Downloading violin weight file...');
+    websave('violinWeights.mat', 'https://github.com/dsuedholt/matlab-ddsp-plugin/raw/0.1.0/violinWeights.mat');
+    disp('Done');
+end
 validateAudioPlugin violinPlugin;
 generateAudioPlugin violinPlugin;
 
+if ~isfile('saxophoneWeights.mat')
+    disp('Downloading saxophone weight file...');
+    websave('saxophoneWeights.mat', 'https://github.com/dsuedholt/matlab-ddsp-plugin/raw/0.1.0/saxophoneWeights.mat');
+    disp('Done');
+end
 validateAudioPlugin saxophonePlugin;
 generateAudioPlugin saxophonePlugin;
 
+if ~isfile('fluteWeights.mat')
+    disp('Downloading flute weight file...');
+    websave('fluteWeights.mat', 'https://github.com/dsuedholt/matlab-ddsp-plugin/raw/0.1.0/fluteWeights.mat');
+    disp('Done');
+end
 validateAudioPlugin flutePlugin;
 generateAudioPlugin flutePlugin;
 
+if ~isfile('trumpetWeights.mat')
+    disp('Downloading trumpet weight file...');
+    websave('trumpetWeights.mat', 'https://github.com/dsuedholt/matlab-ddsp-plugin/raw/0.1.0/trumpetWeights.mat');
+    disp('Done');
+end
 validateAudioPlugin trumpetPlugin;
 generateAudioPlugin trumpetPlugin;
